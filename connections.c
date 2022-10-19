0b224a84f011369ce8ea8767cd2a520652a14c4a
jtchan2 <jtchan2@dons.usfca.edu>
2022-10-18 20:31:09 -0700

    Created socket

--- connections.c
+++ connections.c
@@ -5,7 +5,20 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
-
+#define PORT 8080
 int main (int argc, char *argv[]){
+	int socketfd;
+	struct sockaddr_in server;
 
+	//creating socket
+	socketfd = socket(AF_INET , SOCK_STREAM, 0);
+	if(socketfd== -1){
+		printf("Could not Create socket");
+	}
+
+	//preparing the Sockaddr_in struct
+	server.sin_family = AF_INET;
+
+
+	return 0;
 }
